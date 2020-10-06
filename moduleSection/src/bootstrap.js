21d5125efa23b048004e873aeb646316f590fbe7
evalynledezma <evalynledezmacode@gmail.com>
2020-10-06 17:01:01 -0600

    multiple and default exports

--- moduleSection/src/bootstrap.js
+++ moduleSection/src/bootstrap.js
@@ -1,2 +1,11 @@
-// ES6 code goes here
-console.log('Hi there');
+// How to Import Multiple Elements from a JavaScript File
+// import * as helper  from './helper';
+
+// console.log(helper.greeting);
+// console.log(helper.multiply(5, 6));
+
+
+// Guide to Default Exports in JavaScript
+import navigation, { greeting, multiply} from './navigation';
+
+console.log(greeting);
---
+++ moduleSection/src/helper.js
@@ -0,0 +1,6 @@
+export function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+export const greeting = 'Hi there';
+
